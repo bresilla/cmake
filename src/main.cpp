cc9c99fc1dc85b921b3a9b75310d7bc1d0af4472
Trim Bresilla <trim.bresilla@gmail.com>
2019-08-23 20:18:26 +0000

    cmake cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,10 @@
 #define LOG(x) std::cout << x << std::endl
 
 int main() {
+
     Log log;
     log.setLevel(Log::LevelInfo);
     log.INFO("Sarting info here");
     log.WARN("starting warning here");
     log.ERROR("starting error here");
-
-    //Player player(5, 5, 2);
-    //player.Move(3, 5);
-    //increment();
-    //increment();
-    //increment();
-    //increment();
-
 }
