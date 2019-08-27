1ad2c31032748795aaf4868fba4b1da4ecb6fb72
Trim Bresilla <trim.bresilla@gmail.com>
2019-08-27 23:19:30 +0000

    more cmake cleanup and better strucutre

--- src/inc/Log.h
+++ src/inc/Log.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+// #include "fmt/format.h"
 
 class Log{
     public:
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include "inc/Log.h"
+#include "fmt/format.h"
+// #include "spdlog/spdlog.h"
 
 #define LOG(x) std::cout << x << std::endl
 
 int main() {
 
-    Log log;
-    log.setLevel(Log::LevelInfo);
-    log.INFO("Sarting info here");
-    log.WARN("starting warning here");
-    log.ERROR("starting error here");
+
+    std::string s = fmt::format("I'd rather be {1} than {0}.", "right", "happy");
+    fmt::print(s);  // Python-like format string syntax
+
+
+    // Log log;
+    // log.setLevel(Log::LevelInfo);
+    // log.INFO("Sarting info here");
+    // log.WARN("starting warning here");
+    // log.ERROR("starting error here");
 }
