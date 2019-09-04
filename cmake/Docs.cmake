d924e2bfca8a3397fe456c066877739ed2b0f026
Trim Bresilla <trim.bresilla@gmail.com>
2019-09-04 15:09:12 +0000

    doxygen fixed

--- cmake/Docs.cmake
+++ cmake/Docs.cmake
@@ -1,10 +1,11 @@
 ###### DOXYFILE ######
 if(BUILD_DOXYGEN_DOCS)
     find_package(Doxygen)
-    set(DOXYGEN_OUTPUT_DIR ${CMAKE_CURRENT_BINARY_DIR}/docs/doxygen)
+    set(DOXYGEN_INPUT_DIR ${PROJECT_PATH}/src)
+    set(DOXYGEN_OUTPUT_DIR ${PROJECT_BUILD}/docs/doxygen)
     set(DOXYGEN_INDEX_FILE ${DOXYGEN_OUTPUT_DIR}/html/index.html)
     set(DOXYFILE_IN ${PROJECT_PATH}/etc/Doxyfile.in)
-    set(DOXYFILE_OUT ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
+    set(DOXYFILE_OUT ${PROJECT_BUILD}/Doxyfile)
     configure_file(${DOXYFILE_IN} ${DOXYFILE_OUT} @ONLY)
     add_custom_command(OUTPUT ${DOXYGEN_INDEX_FILE}
                     COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYFILE_OUT}
@@ -21,7 +22,7 @@ if(BUILD_SPHINX_DOCS)
     # create a "read" command to use with: make read
     set(RTD_TARGET_NAME "read")
     set(SPHINX_SOURCE ${CMAKE_CURRENT_SOURCE_DIR})
-    set(SPHINX_BUILD ${CMAKE_CURRENT_BINARY_DIR}/sphinx)
+    set(SPHINX_BUILD ${PROJECT_BUILD}/sphinx)
     set(SPHINX_INDEX_FILE ${SPHINX_BUILD}/index.html)
     add_custom_command(OUTPUT ${SPHINX_INDEX_FILE}
                     COMMAND
@@ -29,7 +30,7 @@ if(BUILD_SPHINX_DOCS)
                         # Tell Breathe where to find the Doxygen output
                         -Dbreathe_projects.CatCutifier=${DOXYGEN_OUTPUT_DIR}/xml
                     ${SPHINX_SOURCE} ${SPHINX_BUILD}
-                    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+                    WORKING_DIRECTORY ${PROJECT_BUILD}
                     DEPENDS
                     # Other docs files you want to track should go here (or in some variable)
                     ${CMAKE_CURRENT_SOURCE_DIR}/index.rst
