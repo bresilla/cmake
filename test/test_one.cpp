054b97f935c7ff667b4577b7c5a050c6b5b8a14e
Trim Bresilla <trim.bresilla@gmail.com>
2019-11-02 14:29:14 +0100

    Removed folder inside source directory

--- test/test_one.cpp
+++ test/test_one.cpp
@@ -9,3 +9,16 @@ TEST_CASE( "testing the factorial function" ) {
     CHECK( utl::factorial( 3 ) == 6 );
     CHECK( utl::factorial( 10 ) == 3628800 );
 }
+
+
+TEST_CASE( "testing shape class" ) {
+    Object shape;
+    shape.setSurface( 5, 4 );
+    CHECK( shape.getSurface() == 20 );
+}
+
+TEST_CASE( "testing shape class" ) {
+    Object shape;
+    shape.setVolume( 2, 3, 4 );
+    CHECK( shape.getVolume() == 24 );
+}
