39e7ef9e49c8c4149fd75779e0bec186f24a7de7
Trim Bresilla <trim.bresilla@gmail.com>
2019-11-05 14:53:17 +0100

    ccache with CMAKE and ninja build

--- shell.nix
+++ shell.nix
@@ -2,6 +2,7 @@ with import <nixpkgs> {}; rec {
   cplateEnv = stdenv.mkDerivation {
     name = "cmake";
     buildInputs = [ stdenv
+                    ccache
                     cmake
                     pkgconfig
                     gcc
@@ -19,7 +20,6 @@ with import <nixpkgs> {}; rec {
                     ncurses
                   ];
     LD_LIBRARY_PATH="/run/opengl-driver/lib;${xorg.libX11}/lib/;${libGL}/lib/;${libGLU}/lib;${freeglut}/lib";
-    TOOLSET="clang++-7.1";
-    LLVM_CONFIG_BINARY="/nix/store/jkjzhbfmf2miplcz0svy5c8yd01qsg1l-llvm-7.1.0/bin/llvm-config";
+    CXX="clang++";
   };
 }
