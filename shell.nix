f72b8ceefab6653e8ced0b10a4a0cd19b021e32a
Trim Bresilla <trim.bresilla@gmail.com>
2019-11-19 15:11:37 +0100

    Cmake useing some default PATH variables && NIX shell added variables
    Variables CPLUS_INCLUDE_PATH and LD_LIBRARY_PATH included in nixshell
    and CMAKE updated to respect that (and include in other libraries)

--- shell.nix
+++ shell.nix
@@ -2,24 +2,24 @@ with import <nixpkgs> {}; rec {
   cplateEnv = stdenv.mkDerivation {
     name = "cmake";
     buildInputs = [ stdenv
-                    ccache
-                    cmake
                     pkgconfig
                     gcc
-                    gdb
-                    clang
-                    clang-tools
                     llvm
-                    llvmPackages.libclang
+                    clang
+                    gdb
                     lldb
+                    cmake
                     ctags
+                    cquery
+                    ccache
                     cppcheck
+                    clang-tools
                     valgrind
                     kcov
                     xorg.libX11
                     ncurses
                   ];
-    LD_LIBRARY_PATH="/run/opengl-driver/lib;${xorg.libX11}/lib/;${libGL}/lib/;${libGLU}/lib;${freeglut}/lib";
-    CXX="clang++";
+    LD_LIBRARY_PATH="${xorg.libX11}/lib/";
+    CPLUS_INCLUDE_PATH="/nix/store/bhngps8y3sf2hdfkbi16bk2ya3k67rkq-gcc-8.3.0/include/c++/8.3.0";
   };
 }
